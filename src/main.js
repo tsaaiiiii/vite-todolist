977b08fe390fc48e67521a787f42eacd2b359a00
tsaaiiiii <a0909513991@gmail.com>
2023-06-01 09:43:21 +0800

    修改分析代碼

--- src/main.js
+++ src/main.js
@@ -2,13 +2,23 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VueGtag from 'vue-gtag'
 
 import App from './App.vue'
 import router from './router'
+const vueGTagSettings = {
+  config: { id: `${import.meta.env.VITE_GA4_MEASUREMENT_ID}` }
+}
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+app.use(VueGtag, vueGTagSettings, router)
+
+import { event } from 'vue-gtag'
+event('export_order_btn', {
+  event_label: 'Export Data Xlsx',
+  event_value: 1
+})
 
 app.mount('#app')
